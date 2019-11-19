72d8069ff5bf422a8fb24a058504024fe03dac8e
Ross Hathaway <hathaway.ross@gmail.com>
2019-11-19 23:52:00 +0200

    parse uploaded table and start to test it for proper formatting

--- server/index.js
+++ server/index.js
@@ -2,13 +2,16 @@ const express = require('express');
 // const compression = require('compression');
 // const cors = require('cors');
 // const morgan = require('morgan');
+const fs = require('fs')
 const path = require('path');
 // const sqlite3 = require('sqlite3').verbose();
 // var db = new sqlite3.Database('./db/carousel.db');
 const fileUpload = require('express-fileupload');
+const readXlsxFile = require('read-excel-file/node');
+
 const app = express();
 const port = process.env.PORT || 3000
-
+const columnTitles = ['CustomerID', 'SeasonID', 'Date', 'Amount']
 
 app.use(express.static('client'))
 app.use(fileUpload({
@@ -26,14 +29,38 @@ app.post('/upload', function (req, res) {
   }
 
   let repayments = req.files.repayments;
-  // console.log('file:', repayments.name)
-  // repayments.mv(`../repayments/${repayments.name}`, function (err) {
-  //   if (err)
-  //     return res.status(500).send(err);
 
-  //   });
-  res.sendFile(repayments.tempFilePath);
+  readXlsxFile(fs.createReadStream(repayments.tempFilePath))
+    .then((rows) => {
+      console.log(rows)
+      let headerRow
+
+      for (let i = 0; i < rows.length; i++) {
+        const currentRow = rows[i]
+
+        if (!Array.isArray(currentRow)) res.end(`Please upload an excel .xslx doc with all required columns and titles for each column from the following list: ${columnTitles}`)
+
+        // check to make sure there is a header row with proper headers and save it for future reference
+        if (i === 0) {
+          if (!currentRow.every(
+            (cell) => columnTitles.indexOf(cell) > -1)) {
+            res.end(`Please upload an excel .xslx doc with all required columns and titles for each column from the following list: ${columnTitles}`)
+          }
+          headerRow = currentRow
+        } else {
+
+          // compare with appropriate rows in db
+          // save 
+          // send report
+        }
+      }
+    })
+
+  // res.sendFile(repayments.tempFilePath);
+  res.end()
 });
+
+
 
 
 app.listen(port, () => console.log(`server is listening on port ${port}`))
