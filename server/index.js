bd2d4a96dd72a352921cb579d47a948c030560f1
Ross Hathaway <hathaway.ross@gmail.com>
2019-11-19 11:21:14 +0200

    have working file upload, which will send file back to user for testing

--- server/index.js
+++ server/index.js
@@ -11,21 +11,28 @@ const port = process.env.PORT || 3000
 
 
 app.use(express.static('client'))
-app.use(fileUpload());
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: '/tmp/'
+}));
 // app.use(compression());
 // app.use(cors());
 // app.use(morgan('dev'));
 
 
 app.post('/upload', function (req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
   let repayments = req.files.repayments;
+  // console.log('file:', repayments.name)
+  // repayments.mv(`../repayments/${repayments.name}`, function (err) {
+  //   if (err)
+  //     return res.status(500).send(err);
 
-  repayments.mv(`../repayments/${repayments.name}`, function (err) {
-    if (err)
-      return res.status(500).send(err);
-
-    res.send('File uploaded!');
-  });
+  //   });
+  res.sendFile(repayments.tempFilePath);
 });
 
 
